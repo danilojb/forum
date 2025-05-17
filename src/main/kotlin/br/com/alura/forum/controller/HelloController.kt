9cb3102a8808c2af06e0085b46f3be0ff4fe810b
danilojb <danilojb@gmail.com>
2025-05-17 17:35:13 -0300

    Adding Tipoco controller

--- src/main/kotlin/br/com/alura/forum/controller/HelloController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/HelloController.kt
@@ -1,4 +1,4 @@
-package br.com.alura.forum
+package br.com.alura.forum.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/hello")
 class HelloController {
+
     @GetMapping
     fun hello(): String{
         return "Hello World!!"
+
     }
 }
